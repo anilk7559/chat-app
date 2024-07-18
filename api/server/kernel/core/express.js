c94c7606a577adcbb095262ce82f253478f61352
MuzamilEng <muzekhan777@gmail.com>
2024-07-18 14:10:11 +0500

    added cors

--- api/server/kernel/core/express.js
+++ api/server/kernel/core/express.js
@@ -30,6 +30,14 @@ exports.core = async (kernel) => {
   // Enable CORS for all origins
   kernel.app.use(cors({ origin: "*" }));
 
+  // Custom CORS headers
+  kernel.app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+    next();
+  });
+
   kernel.app.use(bodyParser.urlencoded({ extended: false }));
   kernel.app.use(bodyParser.json());
   kernel.app.use(methodOverride());
