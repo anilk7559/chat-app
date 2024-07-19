d9a6783c805a342d3459f525a2a28e9ca8badef5
MuzamilEng <muzekhan777@gmail.com>
2024-07-20 03:49:58 +0500

    updated env

--- user/next.config.js
+++ user/next.config.js
@@ -1,85 +1,72 @@
+/** @type {import('next').NextConfig} */
 const path = require('path');
 const { i18n } = require('./next-i18next.config');
-
-
-module.exports = {
+const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
   }, 
   i18n,
-  distDir: 'dist/.next',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors. in development we need to run yarn lint
-    ignoreDuringBuilds: true
+    ignoreDuringBuilds: true,
   },
   reactStrictMode: true,
   poweredByHeader: false,
   swcMinify: true,
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    API_ENDPOINT: process.env.API_SERVER_ENDPOINT || process.env.API_ENDPOINT
-  },
   publicRuntimeConfig: {
     // Will be available on both server and client
-    API_ENDPOINT: process.env.API_ENDPOINT,
-    SOCKET_ENDPOINT: process.env.SOCKET_ENDPOINT
+    API_ENDPOINT: process.env.API_ENDPOINT || 'https://chat-app-eaxp.onrender.com/v1',
+    SOCKET_ENDPOINT: process.env.SOCKET_ENDPOINT || 'https://chat-app-eaxp.onrender.com',
   },
   async rewrites() {
     return [
       {
         source: '/',
-        destination: '/auth/home'
+        destination: '/auth/home',
       },
       {
         source: '/favorites',
-        destination: '/models?friendOnly=1'
+        destination: '/models?friendOnly=1',
       },
       {
         source: '/login',
-        destination: '/auth/login'
+        destination: '/auth/login',
       },
       {
         source: '/register',
-        destination: '/auth/register'
+        destination: '/auth/register',
       },
       {
         source: '/forgot',
-        destination: '/auth/forgot'
+        destination: '/auth/forgot',
       },
       {
         source: '/token-history',
-        destination: '/tokens/history'
+        destination: '/tokens/history',
       },
       {
         source: '/bookmarked-messages',
-        destination: '/profile/bookmarked-messages'
+        destination: '/profile/bookmarked-messages',
       },
       {
         source: '/purchased-media',
-        destination: '/profile/purchased-media'
+        destination: '/profile/purchased-media',
       },
       {
         source: '/payout-request',
-        destination: '/profile/payout-request'
+        destination: '/profile/payout-request',
       },
       {
         source: '/media-content',
-        destination: '/profile/media-content'
+        destination: '/profile/media-content',
       },
       {
         source: '/payout-account',
-        destination: '/profile/setting/payout-account'
-      }
+        destination: '/profile/setting/payout-account',
+      },
     ];
-  }
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/auth/login',
-  //       permanent: true
-  //     }
-  //   ]
-  // }
+  },
 };
+
+module.exports = nextConfig;
